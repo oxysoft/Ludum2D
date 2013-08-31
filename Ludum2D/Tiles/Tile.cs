30261565722c987254406cf5e30a35b92e861f08
oxysoft <petitas007@yahoo.ca>
2013-08-31 14:35:09 -0700

    Added tiles and tile mapping

---
+++ Ludum2D/Tiles/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Ludum2D.Tiles {
+	public abstract class Tile {
+		public static int Width = 16;
+
+		public static int Height {
+			get { return Width; }
+		}
+
+		public static Vector2 Size {
+			get { return new Vector2(Width, Height); }
+		}
+
+		public Vector2 Location;
+		public abstract int Id { get; }
+
+		public abstract void Update(GameTime gameTime);
+		public abstract void Draw(GameTime gameTime);
+	}
+}
---
+++ Ludum2D/Tiles/TileMapping.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Ludum2D.Tiles {
+	public class TileMapping {
+		private Dictionary<int, Type> hash = new Dictionary<int,Type>();
+
+		public TileMapping() {
+		}
+
+		/// <summary>
+		/// Returns the type mapped to the hex color c
+		/// </summary>
+		/// <param name="c">The color in hexadecimal. note: Alpha channel not supported</param>
+		/// <returns></returns>
+		public Type Get(int c) {
+			if (hash.ContainsKey(c)) {
+				return hash[c];
+			}
+			throw new NotImplementedException(string.Format("There is no tile type mapped to the color '{0}'", c.ToString("X6")));
+		}
+
+		/// <summary>
+		/// Map a color to a tile
+		/// </summary>
+		/// <param name="c">The color in hexadecimal. note: Alpha channel not supported</param>
+		/// <param name="t">The type of the tile. (e.g: typeof(GrassTile))</param>
+		public TileMapping Insert(int c, Type t) {
+			hash.Add(c, t);
+			return this;
+		}
+
+	}
+}
