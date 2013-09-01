445a270fa79be3a59ee661a048c61f5c0d78aa08
oxysoft <petitas007@yahoo.ca>
2013-08-31 23:04:59 -0700

    Added LudumCore class

---
+++ Ludum2D/Core/LudumCore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ludum2D.Tiles;
+using Ludum2D.World;
+using Microsoft.Xna.Framework;
+
+namespace Ludum2D.Core {
+	public class LudumCore {
+		private static LudumCore instance;
+
+		public static LudumCore Instance {
+			get {
+				if (instance == null) {
+					instance = new LudumCore();
+				}
+				return instance;
+			}
+		}
+
+		private LudumCore() {
+		}
+
+		public bool Initialized { get; private set; }
+
+		/// <summary>
+		/// Initialize the cores of the engine
+		/// </summary>
+		public void Initialize() {
+		}
+
+		/// <summary>
+		/// Sets the size of a tile
+		/// </summary>
+		/// <param name="sq">Width and height of a tile; Must be x^2</param>
+		public void SetTileSize(int sq) {
+			if (!Initialized) {
+				throw new Exception("The tile size cannot be modified once the the LudumCore has been initialized");
+			}
+			Tile.Width = sq;
+		}
+	}
+}
